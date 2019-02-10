a846ae8dea8151320e465da99c2c8a91406dd173
Jongik Yoon <yji1221@gmail.com>
2019-02-11 01:03:18 +0900

    added code heart & heart-broken

---
+++ src/components/GameInfo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="gameInfo" class="gameInfo">
+    <div class="section">
+      <span :key="index" v-for="index in 5" class="heart" :class="{broken: index <= 5 - life}" >
+        <font-awesome-icon v-if="index > 5 - life" icon="heart" />
+        <font-awesome-icon v-else icon="heart-broken" />
+      </span>
+    </div>
+    <div class="section">
+      LEVEL<span class="number">{{ level + 1 }}</span>
+    </div>
+    <div class="section">
+      TILES<span class="number">{{ correctTileCount }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gameInfo',
+  props: {
+    level: {
+      type: Number,
+      default: 0
+    },
+    correctTileCount: {
+      type: Number,
+      default: 0
+    },
+    life: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style>
+.gameInfo {
+  text-align: right;
+}
+.gameInfo .section {
+  display: inline-block;
+  position: relative;
+  padding: 2px 10px;
+  margin-right: 3px;
+  font-size: 15px;
+  font-weight: bolder;
+  background: #f2f4f7;
+}
+.gameInfo .section:last-child {
+  margin-right: 0;
+}
+.gameInfo .number {
+  margin-left: 10px;
+}
+.heart {
+  margin-right: 2px;
+  transition: opacity .5s ease;
+}
+.heart.broken {
+  opacity: 0.2;
+}
+</style>
